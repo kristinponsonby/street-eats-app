22a91eeef607f6fed45a4d0cf947aa5d0b3dadaf
Kristin Ponsonby <kristinponsonby@gmail.com>
2021-07-20 22:08:22 -0400

    Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,129 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#FoodTruck.create(
-#name: "Captain Muchachos"
-#cuisine: "Mexican, Tacos"
-#image_url: "https://images.squarespace-cdn.com/content/v1/588fbe2bebbd1a5103f815b8/1615392054113-GJN9VR5U3WR9ZCU5XAQ4/CM-Logo-04.png?format=1500w"
-#gluten_free: true
-#vegetarian: true
-#schedule: "https://www.facebook.com/captainmuchachos/")
+FoodTruck.create(
+name: "Captain Muchachos",
+schedule: "https://www.facebook.com/captainmuchachos/",
+image_url: "https://images.squarespace-cdn.com/content/v1/588fbe2bebbd1a5103f815b8/1615392054113-GJN9VR5U3WR9ZCU5XAQ4/CM-Logo-04.png?format=1500w",
+kind_of_food: "Mexican, Tacos",
+gluten_free: true,
+vegetarian: true)
+
+FoodTruck.create(
+name: "Ramen Bones",
+schedule: "https://www.ramenbones.net/calendar?view=calendar&month=07-2021",
+image_url: "https://images.squarespace-cdn.com/content/v1/5c0f217af2e6b17988e99a21/1544495726516-9TSS0NM75U7ZIALU9T96/unnamed.png?format=1500w",
+kind_of_food: "Japanese, Ramen",
+gluten_free: false,
+vegetarian: true)
+
+FoodTruck.create(
+name: "Stick in a Box",
+schedule: "https://www.facebook.com/stickinaboxknox/",
+image_url: "https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/32105484_162941494384764_1835016268885786624_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=174925&_nc_ohc=eVsUu1IWG0MAX8dCCDb&_nc_ht=scontent-atl3-2.xx&oh=8e5352d24b6dc9fb8f5780635696ccfd&oe=60FC9B54",
+kind_of_food: "Kabobs, Burgers, Brats",
+gluten_free: true,
+vegetarian: true)
+
+FoodTruck.create(
+name: "Knox Wurst",
+schedule: "https://knoxwurst.com/index.php/calendar/",
+image_url: "https://scontent-atl3-2.xx.fbcdn.net/v/t1.18169-9/30582442_150343085799894_4522431624149781745_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=174925&_nc_ohc=OVX8dx4p6K0AX_6m4-r&_nc_ht=scontent-atl3-2.xx&oh=f11f27e272cbca321d54e30ac00ab573&oe=60FBA347",
+kind_of_food: "German, Brats, Sausages",
+gluten_free: true,
+vegetarian: true)
+
+FoodTruck.create(
+name: "Oakwood BBQ",
+schedule: "https://www.facebook.com/OAKWOODBBQKNOX/",
+image_url: "https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/169736007_3477112449060614_3061825399990782558_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=e3f864&_nc_ohc=v5NyBFJPRjEAX90NYZ_&_nc_ht=scontent-atl3-2.xx&oh=e39d07210013046e19a9d6b2f868863f&oe=60FBCFD9",
+kind_of_food: "German, Brats, Sausages",
+gluten_free: true,
+vegetarian: true)
+
+FoodTruck.create(
+name: "Wood Oven Eats",
+schedule: "https://m.facebook.com/woodoveneatscatering/",
+image_url: "https://cdn0.weddingwire.com/emp/fotos/3/1/9/8/6/0/t40_img-2223_51_1068913-1559350587.jpeg",
+kind_of_food: "Pizza, Wood Fired, Italian",
+gluten_free: true,
+vegetarian: true)
+
+FoodTruck.create(
+name: "Chef Aaron's Creations",
+schedule: "https://www.facebook.com/chefaaronscreations/",
+image_url: "https://scontent-atl3-2.xx.fbcdn.net/v/t1.18169-9/22050122_1340449932743699_2286646362929462126_n.jpg?_nc_cat=101&ccb=1-3&_nc_sid=8bfeb9&_nc_ohc=rHjS3pZlwSwAX_8jcu7&_nc_ht=scontent-atl3-2.xx&oh=6637676355d7858c930de9643c2aedd5&oe=60FCB654",
+kind_of_food: "American, Burgers, Tacos",
+gluten_free: false,
+vegetarian: false)
+
+FoodTruck.create(
+name: "The Weenie Waggin'",
+schedule: "https://www.facebook.com/TheWeenieWaggin/",
+image_url: "https://scontent-atl3-2.xx.fbcdn.net/v/t1.6435-9/68426686_1513688462104195_1325298475138547712_n.png?_nc_cat=110&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=bmIdXLhW8tIAX-4xndL&_nc_ht=scontent-atl3-2.xx&oh=12ca2a27f6cce77a7a2fef56611f8c3e&oe=60FC45AD",
+kind_of_food: "American, hot dogs",
+gluten_free: true,
+vegetarian: false)
+
+FoodTruck.create(
+name: "Fai Thai",
+schedule: "https://www.faithaifoodtruck.com/location?view=calendar&month=07-2021",
+image_url: "https://images.squarespace-cdn.com/content/v1/5a9c30ad9d5abb079e2121c4/1603545609986-AKLKXIX9A0TD9IGM6K3L/Fai+Thai+Logo+-+Color.png?format=1500w",
+kind_of_food: "Thai, Fried Rice",
+gluten_free: true,
+vegetarian: true)
+
+FoodTruck.create(
+name: "Wings on the Pig",
+schedule: "https://www.facebook.com/wingsonthepig/",
+image_url: "https://www.facebook.com/wingsonthepig/photos/a.578183106187044/675359896469364",
+kind_of_food: "American, Wings, Tater Tots",
+gluten_free: true,
+vegetarian: true)
+
+FoodTruck.create(
+name: "Ale Rae Grill",
+schedule: "https://www.facebook.com/AleRaeGourmet",
+image_url: "https://cityviewmag.com/wp-content/uploads/2019/04/AleRae-2363-1024x683.jpg",
+kind_of_food: "Gastropub, American, Burgers, Tacos",
+gluten_free: false,
+vegetarian: false)
+
+FoodTruck.create(
+name: "Just Say Queso",
+schedule: "https://www.facebook.com/justsayqueso/",
+image_url: "https://data.parkbench.com/content/data/businesses/3/2/c/3/8/NjAweDYwMA--_32c387634fa945ef9e50f9ac3fd54a3b.jpg",
+kind_of_food: "Mexican, Quesadillas, Tacos",
+gluten_free: false,
+vegetarian: true)
+
+FoodTruck.create(
+name: "Tootsie Truck",
+schedule: "https://tootsieco.com/tootsie-truck/",
+image_url: "https://tootsietruck.square.site/uploads/b/319db4ed9a7b41c86e56cc5225c0e4b2cffaf003fb930423dcdff0284602c92b/Tootsie%20Co_Logo%20(Rooster)_1624496962.png?width=1600",
+kind_of_food: "American, Wraps, Burgers",
+gluten_free: false,
+vegetarian: true)
+
+
+FoodTruck.create(
+name: "Penne for Your Thoughts",
+schedule: "https://pennetruck.com/upcoming-events/",
+image_url: "https://pennetruck.com/wp-content/uploads/2014/10/2018_penne_sm-18.jpg",
+kind_of_food: "Italian, American, Fusion",
+gluten_free: true,
+vegetarian: true)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
